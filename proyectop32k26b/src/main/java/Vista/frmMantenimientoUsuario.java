73a253ae2a66958ce2bfe4fca68f7291856d50d8
Marco Hernandez <mhernandezt23@miumg.edu.gt>
2026-04-17 13:39:19 -0600

    comentario

--- proyectop32k26b/src/main/java/Vista/frmMantenimientoUsuario.java
+++ proyectop32k26b/src/main/java/Vista/frmMantenimientoUsuario.java
@@ -1,4 +1,4 @@
-// Marco Hernandez
+// Marco Hernandez  se agrego examen a la ventana
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
